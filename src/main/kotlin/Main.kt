f364ba9f7b590e2dc9f5d454b23e9bcd2a0d0ac6
Todd Bensmiller <toddbjta@gmail.com>
2021-01-17 22:48:03 -0600

    added safety check for removed users information getting stored wrong

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,11 @@
+import com.jessecorbett.diskord.api.rest.client.ChannelClient
+import com.jessecorbett.diskord.api.rest.client.GuildClient
 import com.jessecorbett.diskord.api.websocket.model.GatewayIntents
-import com.jessecorbett.diskord.dsl.Bot
-import com.jessecorbett.diskord.dsl.bot
-import com.jessecorbett.diskord.dsl.command
-import com.jessecorbett.diskord.dsl.commands
-import com.jessecorbett.diskord.util.*
-import com.sun.javafx.application.PlatformImpl.startup
+import com.jessecorbett.diskord.dsl.*
+import com.jessecorbett.diskord.util.authorId
+import com.jessecorbett.diskord.util.changeNickname
+import com.jessecorbett.diskord.util.sendMessage
+import com.jessecorbett.diskord.util.words
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import mu.KotlinLogging
@@ -29,12 +30,13 @@ suspend fun main()
 		!getenv().keys.contains(x)
 	}
 	log.info("finished checking envvars")
-	if(missingEnvs.isNotEmpty())
+	if (missingEnvs.isNotEmpty())
 	{
 		println("The following environment variables are missing:")
-		missingEnvs.forEach { println(it)}
+		missingEnvs.forEach { println(it) }
 		exitProcess(1)
 	}
+	log.info("checking database connection")
 
 	val prefix = getenv("PREFIX")
 	val completeReaction = "✅"
@@ -46,8 +48,7 @@ suspend fun main()
 	log.info("starting bot")
 	bot(getenv("TOKEN"), intents = GatewayIntents.ALL)
 	{
-		startup()
-		{
+		started {
 			log.info("bot startup reached")
 		}
 		commands(prefix)
@@ -65,19 +66,18 @@ suspend fun main()
 			}
 			command("restart")
 			{
-				if(authorId == getenv("OWNERID"))
+				if (authorId == getenv("OWNERID"))
 				{
 					react(completeReaction)
 					restart()
-				}
-				else
+				} else
 				{
 					react(rejectedReaction)
 				}
 			}
 			command("reload-users")
 			{
-				if(authorId == getenv("OWNERID"))
+				if (authorId == getenv("OWNERID"))
 				{
 					if (words.size == 2 && words[1] == "--confirm")
 					{
@@ -101,10 +101,6 @@ suspend fun main()
 										}
 									}
 									channel.removeMessageReaction(id, currentlyWorkingReaction)
-									logToChannel(
-										guildId,
-										"${author.mention} caused a full database pull of this server"
-									)
 									react(completeReaction)
 									log.info("finished user pull for server $guildId")
 								}
@@ -121,38 +117,61 @@ suspend fun main()
 					{
 						react(rejectedReaction)
 					}
-				}
-				else
+				} else
 				{
 					react(rejectedReaction)
 				}
 			}
 			command("source")
 			{
-				reply("You can find the source to this bot here: https://github.com/azulflame/NicknameMemory")
+				reply("You can find the source to this bot here: https://github.com/azulflame/NicknameMemoryKt")
 			}
 		}
 		guildMemberUpdated {
-			addOrUpdateUser(it.user.id, it.nickname ?: "", it.roles.joinToString(","), it.guildId)
-			logToChannel(it.guildId, "${it.user.mention} updated with nick \"${it.nickname}\" and roles ${it.roles.joinToString(",")}")
-			log.info(
-				"user ${it.user.id} on ${it.guildId} updated with nickname \"${it.nickname}\" and roles ${
-					it.roles.joinToString(
-						","
-					)
-				}"
-			)
+			val emptyUpdate = it.nickname.isNullOrBlank() && it.roles.isEmpty() // is this an empty update, possibly removed from guild
+			// if it is not an empty update, short circuit the user check
+			if (!emptyUpdate || clientStore.guilds[it.guildId].hasUser(it.user.id))
+			{
+				addOrUpdateUser(it.user.id, it.nickname ?: "", it.roles.joinToString(","), it.guildId)
+				log.info(
+					"user ${it.user.id} on ${it.guildId} updated with nickname \"${it.nickname}\" and roles ${
+						it.roles.joinToString(
+							","
+						)
+					}"
+				)
+			}
 		}
 		userJoinedGuild {
-			it.user?.id?.let { userID ->
-				getUserInformation(userID, it.guildId)?.let { info ->
+			it.user?.let { user ->
+				getUserInformation(user.id, it.guildId)?.let { info ->
 					clientStore.guilds[it.guildId].let { guild ->
-						guild.changeNickname(userID, info.nick)
-						info.roles.split(",").forEach { role ->
-							guild.addMemberRole(userID, role)
+						var embedtext = ""
+						if (info.nick.isNotBlank())
+						{
+							log.info("changing nickname")
+							guild.changeNickname(user.id, info.nick)
+							embedtext += "Nick updated to \"${info.nick}\"\n"
 						}
-						log.info("User $userID in server ${it.guildId} given nickname \"${info.nick}\" and roles ${info.roles}")
-						logToChannel(it.guildId, "User ${it.user?.mention} in server ${it.guildId} given nickname \"${info.nick}\" and roles ${info.roles}")
+						if (info.roles.isNotBlank())
+						{
+							embedtext += "Roles added:\n"
+
+							info.roles.split(",").forEach { role ->
+								log.info("Adding role $role")
+								guild.addMemberRole(user.id, role)
+								embedtext += "$role <@&${role}>\n"
+							}
+						}
+						log.info("User ${user.id} in server ${it.guildId} given nickname \"${info.nick}\" and roles ${info.roles}")
+						getLogChannel(it.guildId)?.let { channel ->
+							channel.sendMessage("",
+								embed {
+									title = "${user.username}#${user.discriminator} rejoined the server"
+									description = embedtext
+								}
+							)
+						}
 					}
 				}
 			}
@@ -168,11 +187,11 @@ suspend fun main()
  * @param roleString a comma seperated string of the role IDs the user has
  * @param serverId the ID of the server that the user was modified in
  */
-fun addOrUpdateUser(userId: String, nick: String, roleString: String, serverId: String)
+fun addOrUpdateUser(userId: String, nick: String, roleString: String, serverId: String): Int
 {
 	try
 	{
-		val rowsChanged = DB.connection.update(UserTable)
+		var rowsChanged = DB.connection.update(UserTable)
 		{
 			set(it.nickname, nick)
 			set(it.roles, roleString)
@@ -184,17 +203,19 @@ fun addOrUpdateUser(userId: String, nick: String, roleString: String, serverId: 
 		if (rowsChanged == 0)
 		{
 			// insert command
-			DB.connection.insert(UserTable) {
+			rowsChanged = DB.connection.insert(UserTable) {
 				set(it.userID, userId)
 				set(it.nickname, nick)
 				set(it.roles, roleString)
 				set(it.serverID, serverId)
 			}
 		}
+		return rowsChanged
 	} catch (e: SQLException)
 	{
 		log.error("${e.errorCode} encountered while updating user $userId in guild $serverId")
 	}
+	return 0
 }
 
 /**
@@ -232,9 +253,28 @@ fun getUserInformation(userID: String, serverID: String): SimpleMember?
  * Log to provided message to the channel in the settings
  *
  * @param guildId the server to log the message to
- * @param message the message to log
  */
-suspend fun Bot.logToChannel(guildId: String, message: String) =
-	clientStore.guilds[guildId].getChannels().firstOrNull { x -> x.name == getenv("LOGCHANNEL") }?.id?.let {
-		clientStore.channels[it].sendMessage(message)
+suspend fun Bot.getLogChannel(guildId: String): ChannelClient?
+{
+	clientStore.guilds[guildId].getChannels()
+		.firstOrNull { x -> x.name == getenv("LOGCHANNEL") }?.id?.let { return clientStore.channels[it] }
+	return null
+}
+
+/**
+ * Returns if the user is in the guild of the GuildClient
+ *
+ * @param userID userID of
+ * @return true if the user is in the guild. false if the user is not, or any error occurs
+ */
+suspend fun GuildClient.hasUser(userID: String): Boolean
+{
+	return try
+	{
+		this.getMember(userID)
+		true
+	} catch (ex: Exception)
+	{
+		false
 	}
+}
